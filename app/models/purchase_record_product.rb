41df43b46b9282f5909e0d0d59e4620766b1984d
Ryonyumura <rnyumura@gmail.com>
2019-05-15 00:45:45 +0900

    Purchase

--- app/models/purchase_record_product.rb
+++ app/models/purchase_record_product.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: purchase_record_products
+#
+#  id                 :integer          not null, primary key
+#  purchase_record_id :integer          not null
+#  product_id         :integer          not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 class PurchaseRecordProduct < ApplicationRecord
   belongs_to :product
   belongs_to :purchase_record
--- test/models/purchase_record_product_test.rb
+++ test/models/purchase_record_product_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: purchase_record_products
+#
+#  id                 :integer          not null, primary key
+#  purchase_record_id :integer          not null
+#  product_id         :integer          not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 require 'test_helper'
 
 class PurchaseRecordProductTest < ActiveSupport::TestCase
